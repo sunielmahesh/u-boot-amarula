926bb17c009c0cc783fb3e7275d6c18a8bac1ad8
Jagan Teki <jagan@amarulasolutions.com>
2019-11-25 19:31:08 +0530

    configs: icorem6rqs: Enable USB gadget
    Enable USB gadget for Engicam i.CoreM6 RQS board.
    
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>
    Change-Id: Ib83aeba8abba943e16b9b7608ea94814b75bb6fc

--- include/configs/imx6-engicam.h
+++ include/configs/imx6-engicam.h
@@ -172,6 +172,14 @@
 # endif
 #endif
 
+/* USB */
+#ifdef CONFIG_USB
+# define CONFIG_EHCI_HCD_INIT_AFTER_RESET
+# define CONFIG_MXC_USB_PORTSC			(PORT_PTS_UTMI | PORT_PTS_PTW)
+# define CONFIG_MXC_USB_FLAGS			0
+# define CONFIG_USB_MAX_CONTROLLER_COUNT	2
+#endif
+
 /* Falcon Mode */
 #ifdef CONFIG_SPL_OS_BOOT
 # define CONFIG_SPL_FS_LOAD_ARGS_NAME	"args"
