0e67a437007f693237c201ec53b31abdfb9381a9
Jagan Teki <jagan@amarulasolutions.com>
2020-05-14 16:37:57 +0530

    mtd: sf: Drop plat from sf_probe
    dm_spi_slave_platdata used in sf_probe for printing
    plat->cs value and there is no relevant usage apart
    from this.
    
    We have enough debug messages available in SPI and SF
    areas so drop this plat get and associated bug statement.
    
    Cc: Simon Glass <sjg@chromium.org>
    Cc: Vignesh R <vigneshr@ti.com>
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/mtd/spi/sf_probe.c
+++ drivers/mtd/spi/sf_probe.c
@@ -140,13 +140,11 @@ static int spi_flash_std_get_sw_write_prot(struct udevice *dev)
 int spi_flash_std_probe(struct udevice *dev)
 {
 	struct spi_slave *slave = dev_get_parent_priv(dev);
-	struct dm_spi_slave_platdata *plat = dev_get_parent_platdata(dev);
 	struct spi_flash *flash;
 
 	flash = dev_get_uclass_priv(dev);
 	flash->dev = dev;
 	flash->spi = slave;
-	debug("%s: slave=%p, cs=%d\n", __func__, slave, plat->cs);
 	return spi_flash_probe_slave(flash);
 }
 
