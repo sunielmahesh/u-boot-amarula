76a53ccde3c54c4e6ddcfa7e0bea769e50943e4e
Suniel Mahesh <sunil@amarulasolutions.com>
2020-07-11 19:33:05 +0530

    rock-pi-n8: HDMI, USB 3.0 storage changes
    HDMI, USB 3.0 changes
    
    Signed-off-by: Suniel Mahesh <sunil@amarulasolutions.com>

--- include/configs/evb_rk3288.h
+++ include/configs/evb_rk3288.h
@@ -6,9 +6,15 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
-#define ROCKCHIP_DEVICE_SETTINGS
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdin=serial,usbkbd,cros-ec-keyb\0" \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
 #include <configs/rk3288_common.h>
 
 #define CONFIG_SYS_MMC_ENV_DEV 0
+
+#define CONFIG_USB_OHCI_NEW
+#define CONFIG_SYS_USB_OHCI_MAX_ROOT_PORTS 1
 
 #endif
